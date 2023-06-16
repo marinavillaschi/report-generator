4fa59485bd75ce0d8805cf19f780c7a52c1dd3bc
Marina Villaschi <78309614+marinavillaschi@users.noreply.github.com>
2023-06-16 20:58:41 -0300

    🔐 update secrets and simple improvements

--- functions/cron/services/s3_uploader.py
+++ functions/cron/services/s3_uploader.py
@@ -1,20 +1,12 @@
 import boto3
 import json
 from botocore.exceptions import ClientError
-from decouple import config
-
-AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
 
 
 def upload_to_s3(bucket: str, key: str, data_to_upload) -> str:
     """ Uploads file to S3 bucket """
 
-    s3 = boto3.resource(
-        "s3",
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
-        )
+    s3 = boto3.resource("s3")
 
     try:
         response = s3.Bucket(bucket).Object(key).put(Body=json.dumps(data_to_upload))
@@ -23,6 +15,6 @@ def upload_to_s3(bucket: str, key: str, data_to_upload) -> str:
 
     except ClientError as e:
         print(e)
-        return None
+        raise e
 
     return response_date
