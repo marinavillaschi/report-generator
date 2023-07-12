db53187e754cc3fc7b78bf32442d85b4c18a1dc0
Marina Villaschi <78309614+marinavillaschi@users.noreply.github.com>
2023-07-12 09:03:23 -0300

    🚧 s3 function change to invoke glue crawler

---
+++ functions/s3/services/crawler_invoker.py
@@ -0,0 +1,21 @@
+import boto3
+
+from decouple import config
+
+
+GLUE_CRAWLER_NAME = config("GLUE_CRAWLER_NAME")
+
+
+def start_glue_crawler():
+    try:
+        glue = boto3.client(service_name="glue")
+        glue.start_crawler(Name=GLUE_CRAWLER_NAME)
+
+        print("crawler started successfully!")
+
+    except Exception as e:
+        raise Exception({
+            "statusCode": 500,
+            "body": "FAILED starting glue crawler",
+            "error": e
+        })
