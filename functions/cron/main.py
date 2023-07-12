8d0ec4680dcfd107fe1c15eab6a6c8fdda6f5fc7
Marina Villaschi <78309614+marinavillaschi@users.noreply.github.com>
2023-07-12 08:09:58 -0300

    ⚡️ ingest one file per datapoint for better schema

--- functions/cron/main.py
+++ functions/cron/main.py
@@ -24,25 +24,21 @@ def lambda_handler(event, context):
             "error": e
         })
 
-
     print(f"got {len(coins_marked_data)} coin market data successfully")
+    print("Ready to upload them to S3!")
 
-    #TODO
-    # call upload_to_s3 for each data point so the schema makes sense
-    # or upload data as csv or parquet
+    for i, data in enumerate(coins_marked_data):
+        print(f"\n{i+1}/{len(coins_marked_data)}")
 
-    # for data in coins_marked_data:
-        
+        try:
+            response_date = s3_uploader.upload_to_s3(
+                LAKE_BUCKET_NAME,
+                f"raw/coins_market_data/{MARKET_CATEGORY}/{str(date.today())}/{i+1}.json",
+                coins_marked_data
+            )
 
-    try:
-        response_date = s3_uploader.upload_to_s3(
-            LAKE_BUCKET_NAME,
-            f"raw/coins_market_data/{MARKET_CATEGORY}/{str(date.today())}.json",
-            coins_marked_data
-        )
-
-    except:
-        raise Exception({
-            "statusCode": 500,
-            "body": json.dumps(f"FAILED uploading data to S3 on {response_date}"),
-        })
+        except:
+            raise Exception({
+                "statusCode": 500,
+                "body": json.dumps(f"FAILED uploading data to S3 on {response_date}"),
+            })
