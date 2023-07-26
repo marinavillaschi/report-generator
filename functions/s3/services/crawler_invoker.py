51caff89940f3639013936d9d6677d06c979c9e6
Marina Villaschi <78309614+marinavillaschi@users.noreply.github.com>
2023-07-26 13:22:42 -0300

    🥅 verify crawler state (ready) before starting it

--- functions/s3/services/crawler_invoker.py
+++ functions/s3/services/crawler_invoker.py
@@ -9,9 +9,13 @@ GLUE_CRAWLER_NAME = config("GLUE_CRAWLER_NAME")
 def start_glue_crawler():
     try:
         glue = boto3.client("glue")
-        glue.start_crawler(Name=GLUE_CRAWLER_NAME)
 
-        print("crawler started successfully!")
+        crawler_state = glue.get_crawler(Name=GLUE_CRAWLER_NAME)['Crawler']['State']
+        print(f"{GLUE_CRAWLER_NAME} state is {crawler_state}")
+
+        if crawler_state == "READY":
+            glue.start_crawler(Name=GLUE_CRAWLER_NAME)
+            print(f"{GLUE_CRAWLER_NAME} started successfully!")
 
     except Exception as e:
         raise Exception({
